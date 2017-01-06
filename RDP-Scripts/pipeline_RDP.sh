a30aaab8a22aa27fa5abade5fa2cac7ca20c53dd
GMNB <gian.benucci@gmail.com>
2017-01-06 12:11:33 -0500

    Update pipeline_RDP.sh

--- RDP-Scripts/pipeline_RDP.sh
+++ RDP-Scripts/pipeline_RDP.sh
@@ -1,33 +1,31 @@
 ### process raw reads to generate OTUs 
-1_QC_OTU-picking.sh 
+QC_OTU-picking.sh 
 
 ### format general fasta
-python 2_generaldb_to_RDP.py /peay/unite_ref_db/sh_general_release_31.01.2016.fasta
+python generaldb_to_RDP.py /file_to_path/sh_general_release_31.01.2016.fasta
 # outputs:
 sh_general_release_31.01.2016__RDP_taxonomy.txt
 sh_general_release_31.01.2016__RDP.fasta
 
 ### create RDP-Classifier training files
 # train taxonomy
-python 3_lineage2taxonomyTrain.py sh_general_release_31.01.2016__RDP_taxonomy.txt > sh_general_release_31.01.2016__RDP_taxonomy_trained.txt
+python lineage2taxonomyTrain.py sh_general_release_31.01.2016__RDP_taxonomy.txt > sh_general_release_31.01.2016__RDP_taxonomy_trained.txt
 # train fasta 
-python 4_fasta_addFullLineage.py sh_general_release_31.01.2016__RDP_taxonomy.txt  sh_general_release_31.01.2016__RDP.fasta > sh_general_release_31.01.2016__RDP_trained.fasta
+python fasta_addFullLineage.py sh_general_release_31.01.2016__RDP_taxonomy.txt  sh_general_release_31.01.2016__RDP.fasta > sh_general_release_31.01.2016__RDP_trained.fasta
 
 ### train the Classifier
-java -Xmx8g -jar /mnt/research/rdp/public/RDPTools/classifier.jar train -o /peay/RDP_trained/ -s /peay/sh_general_release_31.01.2016__RDP_trained.fasta -t /peay/sh_general_release_31.01.2016__RDP_taxonomy_trained.txt 
+java -Xmx8g -jar /mnt/research/rdp/public/RDPTools/classifier.jar train -o /file_to_path/RDP_trained/ -s /file_to_path/sh_general_release_31.01.2016__RDP_trained.fasta -t /file_to_path/sh_general_release_31.01.2016__RDP_taxonomy_trained.txt 
 
 # download sample files from RDP GitHub page
 # https://github.com/rdpstaff/classifier
-cp samplefiles/rRNAClassifier.properties RDP_trained/
+cp samplefiles/rRNAClassifier.properties /file_to_path/RDP_trained/
 
 ### assign taxonomy
-java -Xmx8g -jar /mnt/research/rdp/public/RDPTools/classifier.jar classify --conf 0.5 --format allrank --train_propfile /peay/RDP_trained/rRNAClassifier.properties --hier_outfile /peay/peay_otu_rdp.txt -o /peay/peay_taxonomy_rdp.txt /peay/peay_otus_sampled.fasta
+java -Xmx8g -jar /mnt/research/rdp/public/RDPTools/classifier.jar classify --conf 0.5 --format allrank --train_propfile /file_to_path/RDP_trained/rRNAClassifier.properties --hier_outfile /file_to_path/peay_otu_rdp.txt -o /file_to_path/peay_taxonomy_rdp.txt /file_to_path/peay_otus_sampled.fasta
 
 ### post-assignment filtering
 ### standardize taxonomy table format
-python 7_filter_rdp.py peay_taxonomy_rdp.txt peay_taxonomy_rdp__final.txt
+python filter_RDP.py peay_taxonomy_rdp.txt peay_taxonomy_rdp__final.txt
 
-### merge all three taxonomy files
-python compare_taxonomies_012.py peay_taxonomy_rdp__final.txt peay_taxonomy_qiime__final.txt peay_taxonomy_utax__final.txt
 
 
