62d0aeda7cfabbdd0932578acb1676d446ab9809
Kristi Gdanetz MacCready <klg5183@gmail.com>
2017-01-09 10:06:04 -0500

    Add files via upload

--- UTAX-Scripts/pipeline_UTAX.sh
+++ UTAX-Scripts/pipeline_UTAX.sh
@@ -1,5 +1,5 @@
 ### process raw reads to generate OTUs 
-1_QC_OTU-picking.sh 
+QC_OTU-picking.sh 
 
 ### format general fasta & create UTAX-DB training files
 python generaldb_to_UTAXdb.py sh_general_release_31.01.2016.fasta
@@ -7,17 +7,14 @@ python generaldb_to_UTAXdb.py sh_general_release_31.01.2016.fasta
 sh_general_release_31.01.2016__UTAX.fasta
 
 ### train the classifier
-cd /mnt/research/rdp/public/thirdParty
-./usearch8.1.1831_i86linux64 -utax_train /path_to_files/unite_ref_db/sh_general_release_31.01.2016__UTAX__filtered.fasta  -report /path_to_files/unite_ref_db/utax_db_31.01.2016_report.txt -taxconfsout /path_to_files/unite_ref_db/utax.31.01.2016.tc -utax_splitlevels NVpcofgs -utax_trainlevels kpcofgs -log /path_to_files/unite_ref_db/utax_train.log -report /path_to_files/unite_ref_db/utax_report.txt
+usearch8 -utax_train /path_to_files/unite_ref_db/sh_general_release_31.01.2016__UTAX__filtered.fasta  -report /path_to_files/unite_ref_db/utax_db_31.01.2016_report.txt -taxconfsout /path_to_files/unite_ref_db/utax.31.01.2016.tc -utax_splitlevels NVpcofgs -utax_trainlevels kpcofgs -log /path_to_files/unite_ref_db/utax_train.log -report /path_to_files/unite_ref_db/utax_report.txt
 
-./usearch8.1.1831_i86linux64 -makeudb_utax /path_to_files/unite_ref_db/sh_general_release_31.01.2016__UTAX.fasta -taxconfsin /path_to_files/unite_ref_db/utax.31.01.2016.tc -output /path_to_files/unite_ref_db/utax_db_31.01.2016.plus_plant.db -log /path_to_files/unite_ref_db/make_udb.log -report /path_to_files/unite_ref_db/utax_report.txt
+usearch8 -makeudb_utax /path_to_files/unite_ref_db/sh_general_release_31.01.2016__UTAX.fasta -taxconfsin /path_to_files/unite_ref_db/utax.31.01.2016.tc -output /path_to_files/unite_ref_db/utax_db_31.01.2016.plus_plant.db -log /path_to_files/unite_ref_db/make_udb.log -report /path_to_files/unite_ref_db/utax_report.txt
 
 ### assign taxonomy
-./usearch8.1.1831_i86linux64 -utax /path_to_files/peay_otus_sampled.fasta -db /path_to_files/unite_ref_db/utax_db_31.01.2016.db -strand both -utaxout /path_to_files/peay_taxonomy_utax.txt -otutabout /path_to_files/peay_otu_utax.txt -rdpout /path_to_files/peay.rdpoutfile.utax.txt -utax_cutoff 0.8 -strand plus -threads 6
+usearch8 -utax /path_to_files/peay_otus_sampled.fasta -db /path_to_files/unite_ref_db/utax_db_31.01.2016.db -strand both -utaxout /path_to_files/peay_taxonomy_utax.txt -otutabout /path_to_files/peay_otu_utax.txt -rdpout /path_to_files/peay.rdpoutfile.utax.txt -utax_cutoff 0.8 -strand plus -threads 6
 
 ### post-assignment filtering
 ### standardize taxonomy table format
-python 6_filter_utax.py peay_taxonomy_utax.txt peay_taxonomy_utax__final.txt
+python filter_UTAX.py peay_taxonomy_utax.txt peay_taxonomy_utax__final.txt
 
-### merge all three taxonomy files
-python compare_taxonomies_012.py peay_taxonomy_rdp__final.txt peay_taxonomy_qiime__final.txt peay_taxonomy_utax__final.txt
