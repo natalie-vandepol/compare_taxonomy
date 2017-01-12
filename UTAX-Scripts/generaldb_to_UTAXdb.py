55aa29c6ffdeaa449681d3e674154df4d9029db4
natalie-vandepol <natalie-vandepol@users.noreply.github.com>
2017-01-12 14:18:18 -0500

    Update generaldb_to_UTAXdb.py

--- UTAX-Scripts/generaldb_to_UTAXdb.py
+++ UTAX-Scripts/generaldb_to_UTAXdb.py
@@ -5,10 +5,11 @@
 #	*  command line: python generaldb_to_UTAXdb.py [path/filename(s)]	#
 #	*  accepts unlimited reference database files and converts them to a	#
 #	   general format							#
-#	*  filters out any taxonomic levels that are "unknown", "unidentified",	#
-#	   or "Incertae_sedis" and any sequences whose kingdom is not "Fungi".	#
+#	*  also creates a filtered database for classifier training, without	#
+#	   taxonomic levels that are "unknown", "unidentified", or		#
+#	   "Incertae_sedis" and any sequences whose kingdom is not "Fungi".	#
 #	*  output files written to original file location, appended		#
-#	   "__UTAX.fasta"							#
+#	   "__UTAX.fasta" and "__UTAX_filtered.fasta"				#
 #################################################################################
 
 import sys, os
@@ -19,20 +20,28 @@ for arg in sys.argv[1:]:
 	gen.close()
 
 	fastatax = open(".".join(os.path.splitext(arg)[:-1])+"__UTAX.fasta","w")
+	filtered = open(".".join(os.path.splitext(arg)[:-1])+"__UTAX_filtered.fasta","w")
+	
 	for i, line in enumerate(all_lines):
 		if line[0]==">":
 			temp0 = line[1:].split("|")
 			utax_name = temp0[1]+"|"+temp0[2]
 			utax_taxa = temp0[4][1:].strip().replace("__",":").replace(";",",")
 			
-			temp_line = ">"+utax_name+";tax=d"+utax_taxa+";\n"
-			temp = temp_line.strip().split(",")
+			temp_line = ">"+utax_name+";tax=d"+utax_taxa+";"
+			fastatax.write(temp_line+"\n")
+			
+			temp = temp_line.split(",")
 			if temp[0].endswith("Fungi"):
 				temp2 = [x for x in temp if "unidentified" not in x]
 				temp3 = [y for y in temp2 if "Incertae_sedis" not in y]
 				temp4 = [z for z in temp3 if "unknown" not in z]
-				utax_taxa = ",".join(temp4)
+				filt_taxa = ",".join(temp4)
 							
-				seq = all_lines[i+1].upper()
-				fastatax.write(utax_taxa+"\n"+seq)
+				filtered.write(filt_taxa+"\n")
+		else:
+			fastatax.write(line)
+			filtered.write(line)
 	fastatax.close()
+	filtered.close()
+
